7237a0517057521d53ae3112a371c88b9b7f93d2
OSCAR RIOS <oscr.rss@gmail.com>
2018-12-15 03:17:42 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,25 @@
-//define functions here
-
+function getIt(){
+  $('p').on('click', function(){
+    window.alert('Hey') ;
+    });
+  }
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+function pressIt(){
+  $("input").on("keydown" , function(g) {
+    if (g.which === 71 ) {
+      window.alert("G was pressed."); 
+    }
+  });
+}
+function submiIt(){
+  $("form").on("submit", function() {
+    window.alert("Your form is going to be submitted now.");
+  });
+}
 $(document).ready(function(){
 
 // call functions here
